c496a95c900feeb35e64a3f26714920d21f27311
Tasim <118922118+TasimDev@users.noreply.github.com>
2024-01-04 20:23:22 +0200

    sdfdsf

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -1,3 +1,39 @@
 
+let headerBtn = document.querySelector('#header-btn');
+let headerMenu = document.querySelector('#header-menu');
+let headerNav = document.querySelector('#header');
+let headerListItem = document.querySelectorAll('.header_list-item');
 
-  
+headerBtn.addEventListener('click', () => {
+  headerMenu.classList.toggle('active');
+  headerBtn.classList.toggle('active');
+  headerNav.classList.toggle('active');
+})
+headerListItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    headerMenu.classList.toggle('active');
+    headerBtn.classList.toggle('active');
+    headerNav.classList.toggle('active');
+  })
+})
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  let width = window.innerWidth;
+  if (width > '1200') {
+    if (scrollTop > lastScrollTop) {
+      headerNav.style.top = '-90px';
+
+    } else {
+      headerNav.style.top = '0';
+    }
+    lastScrollTop = scrollTop;
+  } else {
+    headerNav.style.top = 0;
+  }
+
+})
+
+
