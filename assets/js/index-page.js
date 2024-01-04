9be05f3aa6aa56e76f614c96f4ca2edc01381dab
Tasim <118922118+TasimDev@users.noreply.github.com>
2024-01-04 16:39:40 +0200

    fgzfg

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -1,16 +1,3 @@
-fetch('components/header.html')
-  .then(response => response.text())
-  .then(data => {
-    // Inject the HTML content into the container
-    $('Navbar').html(data);
-  });
-fetch('components/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    // Inject the HTML content into the container
-    $('Footer').html(data);
-  });
-
 
 
   
