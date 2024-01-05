bbf748667bb943367e7573b59ec2e0b8f3f60b2a
Tasim <118922118+TasimDev@users.noreply.github.com>
2024-01-05 21:19:29 +0200

    sdfsdf

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -3,7 +3,7 @@ let headerBtn = document.querySelector('#header-btn');
 let headerMenu = document.querySelector('#header-menu');
 let headerNav = document.querySelector('#header');
 let headerListItem = document.querySelectorAll('.header_list-item');
-
+let headerLogo = document.querySelector('.header__logo #logo')
 headerBtn.addEventListener('click', () => {
   headerMenu.classList.toggle('active');
   headerBtn.classList.toggle('active');
@@ -23,11 +23,12 @@ window.addEventListener("scroll", () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > 100) {
-
     headerNav.classList.add('sticky')
+    headerLogo.setAttribute('src', "assets/images/logo/neotet-white-logo.png")
 
   } else {
     headerNav.classList.remove('sticky');
+    headerLogo.setAttribute('src', "assets/images/logo/neotet-logo.png")
   }
 
 })
