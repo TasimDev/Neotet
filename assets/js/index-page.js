e20ae6e23bcd6a739eb923886c41fe8232587a54
Tasim <118922118+TasimDev@users.noreply.github.com>
2024-01-11 22:21:18 +0200

    wsdfsdaf

--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -4,33 +4,55 @@ let headerMenu = document.querySelector('#header-menu');
 let headerNav = document.querySelector('#header');
 let headerListItem = document.querySelectorAll('.header_list-item');
 let headerLogo = document.querySelector('.header__logo #logo')
-headerBtn.addEventListener('click', () => {
-  headerMenu.classList.toggle('active');
-  headerBtn.classList.toggle('active');
-  headerNav.classList.toggle('active');
-})
+
+
 headerListItem.forEach((item) => {
   item.addEventListener('click', () => {
-    headerMenu.classList.toggle('active');
-    headerBtn.classList.toggle('active');
-    headerNav.classList.toggle('active');
-  })
-})
+    toggleHeader();
+  });
+});
 
 let lastScrollTop = 0;
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", handleScroll);
+
+
+
+const servicesMenu = document.querySelector(".services__menu");
+const serviceLink = document.querySelector("#services-link");
+
+
+
+function handleScroll() {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > 100) {
-    headerNav.classList.add('sticky')
-    headerLogo.setAttribute('src', "assets/images/logo/neotet-white-logo.png")
-
+    headerNav.classList.add('sticky');
+    headerLogo.setAttribute('src', "assets/images/logo/neotet-white-logo.png");
   } else {
     headerNav.classList.remove('sticky');
-    headerLogo.setAttribute('src', "assets/images/logo/neotet-logo.png")
+    headerLogo.setAttribute('src', "assets/images/logo/neotet-logo.png");
   }
+}
 
-})
+function toggleHeader() {
+  headerMenu.classList.toggle('active');
+  headerBtn.classList.toggle('active');
+  headerNav.classList.toggle('active');
+}
+
+function showServicesMenu() {
+
+  servicesMenu.classList.add("active");
+
+}
+
+function closeServicesMenu() {
+  servicesMenu.classList.remove("active");
+
+}
+
+
+headerBtn.addEventListener('click', toggleHeader);
 
 
